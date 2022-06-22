fa6fafcbdd82121c62a820c562db2b3bb54d6bc0
Diego Moreira <diego.moreira529@gmail.com>
2022-06-21 21:48:20 -0300

    Created logic for enter a sketch pixel amount

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 const sketchArea = document.querySelector(".sketch-area");
 
-let pixelsToDisplay = 16;
+let pixelsToDisplay = prompt("Entre com o tamanho do quadro");
 
 for (let i = 0; i < pixelsToDisplay ** 2; i++) {
   const pixel = document.createElement("div");
   pixel.classList.add("blank-pixel");
   sketchArea.appendChild(pixel);
 }
+
+const blankPixel = document.querySelectorAll(".blank-pixel");
+blankPixel.forEach((item) => {
+  item.style.width = `calc(100% / ${pixelsToDisplay})`;
+  item.style.height = `calc(100% / ${pixelsToDisplay})`;
+});
