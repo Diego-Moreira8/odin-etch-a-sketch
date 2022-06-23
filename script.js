a1947762406195a9e9c3c5c87b96ab69011c4d83
Diego Moreira <diego.moreira529@gmail.com>
2022-06-22 21:47:53 -0300

    Created the input for pixel density

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
+const pixelAmount = document.querySelector("#pixel-amount");
 const sketchArea = document.querySelector(".sketch-area");
 
-let pixelsToDisplay = prompt("Entre com o tamanho do quadro");
-
-for (let i = 0; i < pixelsToDisplay ** 2; i++) {
-  const pixel = document.createElement("div");
-  pixel.classList.add("blank-pixel");
-  sketchArea.appendChild(pixel);
+pixelAmount.addEventListener("change", sketchAreaUpdate);
+function sketchAreaUpdate() {
+  // removes all pixels from before
+  while (sketchArea.lastElementChild) {
+    sketchArea.removeChild(sketchArea.lastElementChild);
+  }
+  // create and draw pixels
+  for (let i = 0; i < pixelAmount.value ** 2; i++) {
+    const pixel = document.createElement("div");
+    pixel.classList.add("blank-pixel");
+    sketchArea.appendChild(pixel);
+  }
+  // style pixels to fit in the sketch area
+  const blankPixel = document.querySelectorAll(".blank-pixel");
+  blankPixel.forEach((item) => {
+    item.style.width = `calc(100% / ${pixelAmount.value})`;
+    item.style.height = `calc(100% / ${pixelAmount.value})`;
+  });
 }
-
-const blankPixel = document.querySelectorAll(".blank-pixel");
-blankPixel.forEach((item) => {
-  item.style.width = `calc(100% / ${pixelsToDisplay})`;
-  item.style.height = `calc(100% / ${pixelsToDisplay})`;
-});
